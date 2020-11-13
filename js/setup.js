51e67150d42f1ff51ffdd6099d9e8eea1840b5fc
Joao Rangel <joao.vran@gmail.com>
2020-11-12 21:26:55 -0300

    added carousel using owl-carousel

---
+++ js/setup.js
@@ -0,0 +1,27 @@
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  responsive: {
+    0: {
+      items: 2
+    },
+    450: {
+      items: 3
+    },
+    650: {
+      items: 4
+    },
+    850: {
+      items: 5
+    },
+    1050: {
+      items: 6
+    },
+    1250: {
+      items: 7
+    },
+    1450: {
+      items: 8
+    }
+  }
+})
